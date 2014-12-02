79af96d3aa6677b22cc9541eb3e040e71ec9aa8e
PiHanHsu <pihanhsu@gmail.com>
2014-12-02 10:06:35 +0800

    Initial Commit

---
+++ RegisterPractice1202/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RegisterPractice1202
+//
+//  Created by PiHan Hsu on 2014/12/2.
+//  Copyright (c) 2014年 PiHan Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RegisterPractice1202/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RegisterPractice1202
+//
+//  Created by PiHan Hsu on 2014/12/2.
+//  Copyright (c) 2014年 PiHan Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
